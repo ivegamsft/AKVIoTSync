26b0f92f1cac2e2ec18c9a63639ed1725720f667
Israel vega <ivega@microsoft.com>
2022-10-06 02:02:30 -0400

    fix eventtype

--- src/api/KeyVaultCertificateVersionCreated/__init__.py
+++ src/api/KeyVaultCertificateVersionCreated/__init__.py
@@ -13,7 +13,7 @@ def main(event: func.EventGridEvent):
         'data': event.get_json(),
         'topic': event.topic,
         'subject': event.subject,
-        'event_type': event.eventType,
+        'event_type': event.event_type,
     })
 
     logging.info('Python EventGrid trigger processed an event: %s', result)
